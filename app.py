db5892ff9d1023c716e3c185b9ff73b420877b82
Paulamejia2 <paulacmejias@yahoo.com>
2025-09-12 12:16:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,30 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Cargar dataset
+
 car_data = pd.read_csv('vehicles_us.csv')
 
-# Título
+
 st.title("Análisis de vehículos 🚗")
 
-# Mostrar primeras filas
+
 st.write("Vista previa de los datos:")
 st.dataframe(car_data.head())
 
 hist_button = st.button('Construir histograma') # crear un botón
      
 if hist_button: # al hacer clic en el botón
-         # escribir un mensaje
+       
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
          
-         # crear un histograma
+        
     fig = px.histogram(car_data, x="odometer")
      
-         # mostrar un gráfico Plotly interactivo
+       
     st.plotly_chart(fig, use_container_width=True)
 
 
-# Casilla para scatter
+
 if st.checkbox("Mostrar dispersión precio vs odómetro"):
     fig2 = px.scatter(car_data, x="odometer", y="price", title="Precio vs Odómetro")
     st.plotly_chart(fig2)
